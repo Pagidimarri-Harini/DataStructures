a2acb15cf4327ccefffd0ddd7f982340b2fd6e8a
Pagidimarri-Harini <88580040+Pagidimarri-Harini@users.noreply.github.com>
2022-02-04 01:09:53 -0800

    Update InputOutput.cpp

--- C++/InputOutput.cpp
+++ C++/InputOutput.cpp
@@ -5,7 +5,7 @@ int main()
   char x;
   cout<<;”Enter the character:”;
   cin.get(x);
-  cout << “The given Character is:”
+  cout << “The given Character is:”;
   cout.put(x);
   cout.write("Welcome to Hyderabad", 5);
 }
