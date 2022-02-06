682482d35597e416653c81ac5ca9cb4cb8e56160
Pagidimarri-Harini <88580040+Pagidimarri-Harini@users.noreply.github.com>
2022-02-06 06:18:05 -0800

    Update and rename C++/Filestreams.cpp to C++/Experiment 14/program(b).cpp

--- C++/Experiment 14/program(b).cpp
+++ C++/Experiment 14/program(b).cpp
@@ -3,21 +3,21 @@
 using namespace std;
  int main ()
 {
-  	 char data[100];
+    char data[100];
    	ofstream outfile;
-  	outfile.open("afile.dat");
-cout << "Writing to the file" << endl;
-  	cout << "Enter your name: "; 
-  	cin.getline(data, 100);
-  	outfile << data << endl;
-cout << "Enter your age: "; 
-  	cin >> data;
-  	cin.ignore();
+  	 outfile.open("afile.dat");
+    cout << "Writing to the file" << endl;
+  	 cout << "Enter your name: "; 
+  	 cin.getline(data, 100);
+  	 outfile << data << endl;
+    cout << "Enter your age: "; 
+  	 cin >> data;
+  	 cin.ignore();
    	outfile << data << endl;
    	outfile.close();
    	ifstream infile; 
   	 infile.open("afile.dat"); 
- 	 cout << "Reading from the file" << endl; 
+ 	  cout << "Reading from the file" << endl; 
   	 infile >> data; 
   	 cout << data << endl;
   	 infile >> data; 
