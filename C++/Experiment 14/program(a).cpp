e4b83f57f3a8b4fa3058cb457a3ff50ec5052fda
Pagidimarri-Harini <88580040+Pagidimarri-Harini@users.noreply.github.com>
2022-02-06 06:14:00 -0800

    Update program(a).cpp

--- C++/Experiment 14/program(a).cpp
+++ C++/Experiment 14/program(a).cpp
@@ -1,11 +1,15 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main()
-{
-  char x;
-  cout<<;”Enter the character:”;
-  cin.get(x);
-  cout << “The given Character is:”;
-  cout.put(x);
-  cout.write("Welcome to Hyderabad", 5);
+int main(){
+    char x;
+    cout<<"Enter the character:";
+    cin.get(x);
+    cout<<"The given character is:";
+    cout.put(x);
+    cout<<endl;
+    cout<<"Printing n characters from given sentence:";
+    cout.write("Welcome to Hyderabad", 5);
+    cout<<endl;
+
 }
+
