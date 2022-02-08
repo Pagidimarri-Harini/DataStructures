02a8cef6bb195fdf5a7b4c92c5e23467abdfc996
Pagidimarri-Harini <88580040+Pagidimarri-Harini@users.noreply.github.com>
2022-02-07 21:16:14 -0800

    Create program(c).cpp

---
+++ C++/Experiment 14/program(c).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+int countWords(string str)
+{
+	stringstream s(str);
+	string word;
+
+	int count = 0;
+	while (s >> word)
+		count++;
+	return count;
+}
+int main()
+{
+	string s = "Harini "
+			"I love cpp";
+	cout << " Number of words are: " << countWords(s) << endl;
+	return 0;
+}
